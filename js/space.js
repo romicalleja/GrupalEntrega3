fe6d2589c45d0bb11efde29f3917664a5a1d6f91
juansanpedro <juanignacio50@hotmail.com>
2022-09-07 18:36:01 -0300

    .

--- js/space.js
+++ js/space.js
@@ -1,45 +1,42 @@
-const nasa =  "https://images-api.nasa.gov/";
-const search = document.getElementById("inputBuscar")
-const btn = document.getElementById("btnBuscar")
-let nasaArray =[]
+const nasa = "https://images-api.nasa.gov/";
+const search = document.getElementById("inputBuscar");
+const btn = document.getElementById("btnBuscar");
+let nasaArray = [];
 
-
-function getJSONData (url){
-    let result = {};
-    return fetch(url)
-    .then(response => {
+function getJSONData(url) {
+  let result = {};
+  return fetch(url)
+    .then((response) => {
       if (response.ok) {
         return response.json();
-      }else{
+      } else {
         throw Error(response.statusText);
       }
     })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          return result;
+    .then(function (response) {
+      result.status = "ok";
+      result.data = response;
+      return result;
     })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        return result;
+    .catch(function (error) {
+      result.status = "error";
+      result.data = error;
+      return result;
     });
 }
 
-btn.addEventListener("click", function(e){
-    let nasaimg= nasa + "search=?q"+ search.input
-    getJSONData(nasaimg).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-          console.log(resultObj);
-          nasaArray = resultObj.data;
-          console.log(nasaArray);
-          showresults();
-}
-}
-)
-}
-)
+btn.addEventListener("click", function (e) {
+  let nasaimg = nasa + "search=?q" + search.input;
+  getJSONData(nasaimg).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      console.log(resultObj);
+      nasaArray = resultObj.data;
+      console.log(nasaArray);
+      showresults();
+    }
+  });
+});
 
-function showresults(){
-    return alert("hi")
+function showresults() {
+  return alert("hi");
 }
