6e47e429ccaa0054738be452c53bc9ed5d77cd11
Romina Calleja <romycalleja.12.08@gmail.com>
2022-09-14 18:34:20 -0300

    ,,,,

--- js/space.js
+++ js/space.js
@@ -2,7 +2,8 @@ const nasa =  "https://images-api.nasa.gov/";
 const search = document.getElementById("inputBuscar")
 const btn = document.getElementById("btnBuscar")
 let nasaArray =[]
-
+let items=[]
+const andro= "http://images-api.nasa.gov/search?q=andromeda"
 
 function getJSONData (url){
     let result = {};
@@ -32,33 +33,34 @@ const getnasa = (search) => {
 }
 
 btn.addEventListener("click", function(e){
-  getJSONData(nasa+"search?q="+{search}).then(function (resultObj) {
+  getJSONData(andro).then(function (resultObj) {
         if (resultObj.status === "ok") {
           nasaArray = resultObj.data
-          collection =resultObj.data.collection.items;
+          items =resultObj.data.collection.items;
           console.log(nasaArray);
-          console.log(collection)
+          console.log(items)
           showresults();
 }
 }
-)
-}
-)
+)})
 
 function showresults(){
-let result =""
-let busqueda = collection
+let result ="";
+for(let i= 0; i < items.length;i++)
 {
+  let busqueda = items[i]
+  {
   result+=`<div class="row">
         <div class="col-1-4">
         <div>
-          <img class="bd-placeholder-img card-img-top" src="${busqueda.href}"
-          <h3>${busqueda.title}</h3>
+          <img class="bd-placeholder-img card-img-top" src="${busqueda.links.href}">
+          <h3>${busqueda.data.title}</h3>
           <div>
-            <p>${busqueda.description}</p>
+            <p>${busqueda.data.description}</p>
           </div>
         </div>
       </div
-      `
+      `;
+      document.getElementById("contenedor").innerHTML=result
 }
-}
+}}
